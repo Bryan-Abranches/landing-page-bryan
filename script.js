4a6eccea5039722dfc0ce312846b61ef234c9c35
Bryan-Abranches <bryanabranches5@gmail.com>
2024-08-03 23:23:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+const viewAllProjectsButton = document.getElementById("view-all-projects");
+
+viewAllProjectsButton.addEventListener("click", () => {
+  // Aqui você pode adicionar código para redirecionar para uma página com todos os projetos ou abrir um modal com os projetos.
+  // Por exemplo:
+  window.location.href = "/projetos"; // Redireciona para a página de projetos
+  // ou
+  // openModal(); // Abre um modal com os projetos
+});
